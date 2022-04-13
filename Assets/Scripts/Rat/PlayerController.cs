3e366c10d09232654a0fcaea3f13bff917df374f
Bill Chen <billc6@uci.edu>
2022-04-12 19:01:17 -0700

    Revert "Updated Rat movement"
    This reverts commit d7ee34dd4965ac2067e7ce70b0deb266279fd3a7.

--- Assets/Scripts/Rat/PlayerController.cs
+++ Assets/Scripts/Rat/PlayerController.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer sprite; // REMOVE LATER unless using it for something besides color
     Rigidbody2D rb;
-    [SerializeField] Collider2D standingCollider, crouchingCollider;
     [SerializeField] Transform groundCheckCollider;
-    [SerializeField] Transform ceilingCheckCollider;
     [SerializeField] LayerMask groundLayer;
 
-    [SerializeField] float speed = 5;
-    [SerializeField] float jumpPower = 50;
+    [SerializeField] float speed = 1;
+    [SerializeField] float jumpPower = 500;
     [SerializeField] float groundCheckRadius = 0.2f;
-    [SerializeField] float ceilingCheckRadius = 0.4f;
 
     float horizontalValue;
-    [SerializeField] float crouchSpeedModifier = 0.35f;
     [SerializeField] bool isGrounded;
+    
 
-    [SerializeField] bool isCrouching;
     bool isJumping = false;
 
     void Awake()
@@ -33,26 +28,15 @@ public class PlayerController : MonoBehaviour
         horizontalValue = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump"))
-        {
             isJumping = true;
-            isCrouching = false;
-        }
         else if (Input.GetButtonUp("Jump"))
             isJumping = false;
-
-        if (Input.GetButton("Crouch") && isGrounded)
-            if (Input.GetButton("Jump"))
-                isCrouching = false;
-            else
-                isCrouching = true;
-        else
-            isCrouching = false;
     }
 
     void FixedUpdate()
     {
         GroundCheck();
-        Move(horizontalValue, isJumping, isCrouching);
+        Move(horizontalValue, isJumping);
     }
 
     void GroundCheck()
@@ -62,55 +46,22 @@ public class PlayerController : MonoBehaviour
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
         if (colliders.Length > 0)
         {
-            if (Physics2D.OverlapCircle(ceilingCheckCollider.position, ceilingCheckRadius, groundLayer))
-                isGrounded = false; // redundant but for some reason overlapcircle can't return false idk why
-            else
-                isGrounded = true;
+            isGrounded = true;
         }
     }
 
-    void Move(float dir, bool jumpFlag, bool crouchFlag)
+    void Move(float dir, bool jumpFlag)
     {
-        if (!crouchFlag)
+
+        if (isGrounded && jumpFlag)
         {
-            if (Physics2D.OverlapCircle(ceilingCheckCollider.position, ceilingCheckRadius, groundLayer))
-            {
-                crouchFlag = true;
-            }
-
+            isGrounded = false;
+            jumpFlag = false;
+            rb.velocity = (new Vector2(0f, jumpPower));
         }
-        standingCollider.enabled = !crouchFlag;
-        crouchingCollider.enabled = crouchFlag;
 
-        if (isGrounded)
-        {
-            standingCollider.enabled = !crouchFlag;
-
-            if (jumpFlag)
-            {
-                isGrounded = false;
-                jumpFlag = false;
-                rb.velocity = (new Vector2(0f, jumpPower));
-            }
-        }
         float xVal = dir * speed * 100 * Time.fixedDeltaTime;
-        if (crouchFlag)
-        {
-            xVal *= crouchSpeedModifier;
-            sprite.color = new Color(0, 255, 255); // REMOVE LATER
-        }
-        else
-        {
-            sprite.color = new Color(255, 0, 0); // REMOVE LATER
-        }
-
         Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
         rb.velocity = targetVelocity;
-    }
-
-    void OnDrawGizmosSelected() // This is here just for debugging purposes
-    {
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(ceilingCheckCollider.position, ceilingCheckRadius);
     }
 }
