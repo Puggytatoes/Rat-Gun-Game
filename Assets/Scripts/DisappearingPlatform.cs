11b51a8e8819ca4d6e5a3c288aa9064a9bbe6d6c
Bill Chen <93066475+Puggytatoes@users.noreply.github.com>
2022-04-30 22:02:50 -0700

    added platform code and scuffed conflict fix TestScene
    scuffed

---
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    Rigidbody2D pf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pf = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("TestRat"))
+        {
+            PlatformManager.Instance.StartCoroutine("SpawnPlatform",
+                new Vector2(transform.position.x, transform.position.y));
+            Invoke("DropPlatform", 0.5f);
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    void DropPlatform()
+    {
+        pf.isKinematic = false;
+    }
+   
+}
