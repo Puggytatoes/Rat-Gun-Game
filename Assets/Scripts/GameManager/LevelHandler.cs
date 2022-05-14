e507df678cfbcd3a3ecf7e0805472af3e91c0d37
scho22 <simonc8@uci.edu>
2022-05-14 05:24:40 -0700

    stops for 2sec before next scene

--- Assets/Scripts/GameManager/LevelHandler.cs
+++ Assets/Scripts/GameManager/LevelHandler.cs
@@ -17,11 +17,12 @@ public class LevelHandler : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag =="Player")
+        if (collision.tag == "Player")
         {
+            yield return new WaitForSeconds(2);
             SceneManager.LoadScene("Cutscene");
-        }
+        }   
     }
 }
