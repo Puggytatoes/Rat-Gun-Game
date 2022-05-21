73605950918873465f675ab9ece1c7862529b63f
scho22 <simonc8@uci.edu>
2022-05-21 02:39:52 -0700

    teleport script done, water bug fixed(workaround)

--- Assets/Scripts/[OLD] gdim31 scripts/RatHealth.cs
+++ Assets/Scripts/[OLD] gdim31 scripts/RatHealth.cs
@@ -30,6 +30,16 @@ public class RatHealth : MonoBehaviour
         }
     }
 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("hazard"))
+        {
+            //gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+            //transform.position = gm.lastCheckPointPos;
+            Hurt();
+            Freeze();
+        }
+    }
     public void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
@@ -37,13 +47,7 @@ public class RatHealth : MonoBehaviour
             Hurt();
         }
 
-        if (collision.gameObject.CompareTag("hazard"))
-        {
-            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
-            transform.position = gm.lastCheckPointPos;
-            Hurt();
-            Freeze();
-        }
+        
     }
 
 
