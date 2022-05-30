620b23c5c7c49808aa4a28df5713f95a34fd7681
Bill Chen <93066475+Puggytatoes@users.noreply.github.com>
2022-05-30 00:37:19 -0700

    disabled gun pos stuff (for now)
    GunPosi.cs has been commented out as well (replace bool check with animator.GetBool("IsCrouching")

--- Assets/Scripts/GunPosi.cs
+++ Assets/Scripts/GunPosi.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunPosi : PlayerController
+public class GunPosi : MonoBehaviour
 {
     //float liftSpeed = 10;
     //GameObject target;
@@ -16,6 +16,7 @@ public class GunPosi : PlayerController
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetButton("Crouch"))
         {
             //transform.position = Vector2.Lerp(transform.position, target2.transform.position, Time.deltaTime * liftSpeed);
@@ -30,5 +31,6 @@ public class GunPosi : PlayerController
             pos.y = -3f;
             transform.position = pos;
         }
+        */
     }
 }
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShoot : PlayerController
+public class PlayerShoot : MonoBehaviour
 {
     public float shootSpeed, shootTimer;
 
